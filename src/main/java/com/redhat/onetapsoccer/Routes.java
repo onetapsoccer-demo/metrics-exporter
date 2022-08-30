0d924548e1d1c67cea81e9e9dc3ca622dffaae77
Vinicius Ferraz Campos Florentino <vinicius.ferraz@gmail.com>
2022-08-30 10:17:59 -0300

    finalized

--- src/main/java/com/redhat/onetapsoccer/Routes.java
+++ src/main/java/com/redhat/onetapsoccer/Routes.java
@@ -39,7 +39,52 @@ public class Routes extends RouteBuilder {
             .parameter("metric", "game-start")
             .parameter("query", "sum(com_redhat_onetapsoccer_game_start_total)");
         
-
+        templatedRoute("prometheusTemplate")
+            .parameter("metric", "ground-collision")
+            .parameter("query", "sum(com_redhat_onetapsoccer_ground_collision_total)");
         
+        templatedRoute("prometheusTemplate")
+            .parameter("metric", "pole-collision")
+            .parameter("query", "sum(com_redhat_onetapsoccer_pole_collision_total)");
+
+            templatedRoute("prometheusTemplate")
+            .parameter("metric", "shoot")
+            .parameter("query", "sum(com_redhat_onetapsoccer_shoot_total)");
+
+            templatedRoute("prometheusTemplate")
+            .parameter("metric", "game-over")
+            .parameter("query", "sum(com_redhat_onetapsoccer_game_over_total)");
+
+            templatedRoute("prometheusTemplate")
+            .parameter("metric", "head-collision")
+            .parameter("query", "sum(com_redhat_onetapsoccer_head_collision_total)");
+
+            templatedRoute("prometheusTemplate")
+            .parameter("metric", "goals-per-player")
+            .parameter("query", "sum by (player) (com_redhat_onetapsoccer_total{\"kind\"=\"goal\"})");
+
+            templatedRoute("prometheusTemplate")
+            .parameter("metric", "total-events")
+            .parameter("query", "sum(com_redhat_onetapsoccer_total)");
+
+            templatedRoute("prometheusTemplate")
+            .parameter("metric", "unique-users")
+            .parameter("query", "count(count by (user) (com_redhat_onetapsoccer_game_start_total))");
+            
+            //Dont know why do not work
+            // templatedRoute("prometheusTemplate")
+            // .parameter("metric", "total-collisions")
+            // .parameter("query", "sum(com_redhat_onetapsoccer_pole_collision_total) + sum(com_redhat_onetapsoccer_head_collision_total) + sum(com_redhat_onetapsoccer_ground_collision_total)");
+
+            //Work however need new velocity template
+            // templatedRoute("prometheusTemplate")
+            // .parameter("metric", "total-events-per-kind")
+            // .parameter("query", "sum by (kind) (com_redhat_onetapsoccer_total)");
+
+            //Dont know why do not work
+            // templatedRoute("prometheusTemplate")
+            // .parameter("metric", "ranking-top-10")
+            // .parameter("query", "topk(10, max by (userName) (com_redhat_onetapsoccer_score))");
+
     }
 }
